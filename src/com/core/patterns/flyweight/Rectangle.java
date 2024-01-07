b8ffb17887081b91f07c81fab6c74b65adc9f68a
Klau-Sam <klaudynasamelak@gmail.com>
2024-01-07 23:42:12 +0100

    Flyweight pattern

---
+++ src/com/core/patterns/flyweight/Rectangle.java
@@ -0,0 +1,14 @@
+package com.core.patterns.flyweight;
+
+public class Rectangle extends Shape {
+
+    private String label;
+
+    Rectangle() {
+        label = "rectangle";
+    }
+    @Override
+    public void draw(int length, int breadth, String fillStyle) {
+        System.out.println("Drawing a " + label + " with length " + length + " and breadth " + breadth + ", fill style " + fillStyle);
+    }
+}
---
+++ src/com/core/patterns/flyweight/Test.java
@@ -0,0 +1,8 @@
+package com.core.patterns.flyweight;
+
+public class Test {
+    public static void main(String[] args) {
+        PaintApp app = new PaintApp();
+        app.render(4);
+    }
+}
